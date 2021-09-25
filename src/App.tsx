6cd56589b8ec4c071dea067a3785c406444d52f4
Jeffrey <jeffrey.quan@hotmail.com>
2021-09-25 23:00:42 +1000

    feat: implement auth0 for authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import "./App.css";
+
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+};
+
+const LogoutButton = () => {
+  const { logout } = useAuth0();
+
+  return (
+    <button onClick={() => logout({ returnTo: window.location.origin })}>
+      Log Out
+    </button>
+  );
+};
+
+const Profile = () => {
+  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const serverUrl = process.env.REACT_APP_SERVER_URL;
+
+  const callApi = async () => {
+    try {
+      const response = await fetch(`${serverUrl}/api/movies`);
+
+      const responseData = await response.json();
+
+      console.log(responseData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const callSecureApi = async () => {
+    try {
+      const token = await getAccessTokenSilently();
+
+      const response = await fetch(`${serverUrl}/api/movies/4`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const responseData = await response.json();
+
+      console.log({
+        responseData,
+      });
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
+  return isAuthenticated && user ? (
+    <div>
+      <img src={user.picture} alt={user.name} />
+      <h2>{user.name}</h2>
+      <p>{user.email}</p>
+      <button type="button" className="btn btn-primary" onClick={callSecureApi}>
+        Get Protected Message
+      </button>
+    </div>
+  ) : null;
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
     </div>
   );
 }
---
+++ src/auth/auth-provider.tsx
@@ -0,0 +1,20 @@
+import { WithChildren } from "types";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+// https://auth0.com/docs/quickstart/spa/react/01-login
+// https://auth0.com/blog/complete-guide-to-react-user-authentication/
+
+function AuthProvider({ children }: WithChildren) {
+  return (
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH0_DOMAIN as string}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID as string}
+      redirectUri={window.location.origin}
+      audience={process.env.REACT_APP_AUTH0_AUDIENCE}
+    >
+      {children}
+    </Auth0Provider>
+  );
+}
+
+export default AuthProvider;
