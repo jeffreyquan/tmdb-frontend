42de1f50681cdb4c4fd1f7b7b06535db4cf39059
Jeffrey <jeffrey.quan@hotmail.com>
2021-10-12 21:03:51 +1100

    feat: install and configure tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// https://tailwindcss.com/docs/guides/create-react-app
+
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
