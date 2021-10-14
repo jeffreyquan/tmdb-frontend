fec090964076b847cf2b3c42e4a747c2854f1fcd
Jeffrey <jeffrey.quan@hotmail.com>
2021-10-15 01:36:46 +1100

    feat: add tmdb colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "tmdb-dark-blue": "#0d253f",
+        "tmdb-light-blue": "#01b4e4",
+        "tmdb-light-green": "#90cea1",
+      },
+    },
   },
   variants: {
     extend: {},
