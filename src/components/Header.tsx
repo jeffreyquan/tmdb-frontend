9bb69e964e3e7ee02871ea04ecb9e72a1aa9669f
Jeffrey <jeffrey.quan@hotmail.com>
2021-10-20 21:34:56 +1100

    feat: add link to logo to navigate to home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { ReactComponent as Logo } from "assets/tmdb_blue_short.svg";
+import { Link } from "react-router-dom";
 
 // https://create-react-app.dev/docs/adding-images-fonts-and-files/
 function Header() {
@@ -6,7 +7,9 @@ function Header() {
     <header className="px-4 py-6 bg-tmdb-dark-blue">
       <nav className="flex justify-between">
         <div className="w-32 sm:w-48">
-          <Logo />
+          <Link to="/">
+            <Logo />
+          </Link>
         </div>
         <div></div>
       </nav>
