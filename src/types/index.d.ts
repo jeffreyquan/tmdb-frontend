510c459ec40827cc07cc804c21a1805f80f30069
Jeffrey <jeffrey.quan@hotmail.com>
2021-10-30 02:53:26 +1100

    add actor type

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,5 +1,16 @@
 import * as React from "react";
 
+type Actor = {
+  biography: string | null;
+  dateOfBirth: string | null;
+  dateOfDeath: string | null;
+  id: number;
+  imdbId: string | null;
+  name: string | null;
+  photo: string | null;
+  placeOfBirth: string | null;
+};
+
 type Movie = {
   id: number;
   backdrop: string | null;
@@ -13,4 +24,4 @@ type WithChildren = {
   children: React.ReactNode;
 };
 
-export { Movie, WithChildren };
+export { Actor, Movie, WithChildren };
