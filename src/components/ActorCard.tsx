43a22faabda4eb1967cdff2019a3ed6b796f7f0b
Jeffrey <jeffrey.quan@hotmail.com>
2021-11-10 00:25:24 +1100

    feat: use placeholder image if photo is null

--- src/components/ActorCard.tsx
+++ src/components/ActorCard.tsx
@@ -1,4 +1,5 @@
 import { Actor } from "types";
+import dummyPhoto from "assets/dummy_user_photo.svg";
 
 interface ActorCardProps {
   actor: Actor;
@@ -6,17 +7,18 @@ interface ActorCardProps {
 
 function ActorCard({ actor }: ActorCardProps) {
   const { name, photo } = actor;
+
+  const image = photo ? photo : dummyPhoto;
+
   return (
     <div className="inline-block rounded-2xl shadow-sm">
-      <div className="flex flex-col  w-32">
-        <div className="rounded-t-2xl">
-          {photo ? (
-            <img
-              src={photo}
-              alt={`${name}`}
-              className="m-0 overflow-hidden rounded-t-2xl"
-            />
-          ) : null}
+      <div className="flex flex-col w-32">
+        <div className="flex rounded-t-2xl h-[192px] items-center bg-gray-200">
+          <img
+            src={image}
+            alt={`${name}`}
+            className="m-0 overflow-hidden rounded-t-2xl"
+          />
         </div>
         <p className="text-center my-2">{name}</p>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
+require("tailwindcss/colors");
 // https://tailwindcss.com/docs/guides/create-react-app
 /*
 To override prose styles, add `important: "html"`
 https://github.com/tailwindlabs/tailwindcss-typography/issues/32#issuecomment-663045275
 */
 
+/**
+ * mode: "jit"
+ * https://tailwindcss.com/docs/just-in-time-mode
+ * allows setting styles like this:
+ * h-[128px]
+ */
+
 module.exports = {
+  mode: "jit",
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
